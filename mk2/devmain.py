bf722fb0ede3afc8e8fce054470bd1c7036ca9aa
choijeho <choijeho@naver.com>
2024-10-18 12:02:27 +0900

    new layout

--- mk2/devmain.py
+++ mk2/devmain.py
@@ -42,19 +42,34 @@ with st.sidebar:
 # Main Page center
 main_tab, setting_tab, report_tab = st.tabs(["Main", "Settings", "Report"])
 frame_placeholder = st.empty()
-chart_placeholder = st.empty()
-columns_placeholder = st.empty()
-ear_data = []
-ear_value = 0
+chart_eard_placeholder = st.empty() # dlib 모드에서 ear 차트를 만들기 위한 플레이스홀더
+chart_earm_placeholder = st.empty() # ear(눈) 차트를 만들기 위한 플레이스홀더. 이하 MediaPipe 모드
+chart_mar_placeholder = st.empty() # mar(입) 차트를 만들기 위한 플레이스홀더
+chart_x_placeholder = st.empty() # Head pitching(고개숙임) 차트를 만들기 위한 플레이스홀더
+chart_y_placeholder = st.empty() # Head yawing(좌우쳐다봄) 차트를 만들기 위한 플레이스홀더
+chart_z_placeholder = st.empty() # Head rolling(좌우기울임) 차트를 만들기 위한 플레이스홀더
+columns_placeholder = st.empty() 
+eard_data = []
+eard_value = 0
 frame_order = 0
 vid_file = 'None'
 
 # Streamlit은 버튼입력 시 동영상이 초기화됨 따라서 프레임번호를 기억해야 함
 if 'frame_no' not in st.session_state:
     st.session_state.frame_no = 0  # 처음 시작할 때 프레임 번호는 0
-if ear_data not in st.session_state:
-  st.session_state.chart_data = ear_data
 
+# 차트를 만들기 위한 각 수치들의 저장소 설정
+# 설정하지 않으면 화면 조작시 초기값으로 바뀜
+if 'eard_data' not in st.session_state:
+  st.session_state['eard_data'] = []
+if 'earm_data' not in st.session_state:
+  st.session_state.earm_data = []
+if 'x_data' not in st.session_state:
+  st.session_state.x_data = []
+if 'y_data' not in st.session_state:
+  st.session_state.y_data = []
+if 'z_data' not in st.session_state:
+  st.session_state.z_data = []
 folder_name = "processed"
 if not os.path.exists(folder_name):
   os.mkdir(folder_name)
@@ -65,8 +80,11 @@ with main_tab:
   col_img_org, col_img_proc = st.columns(2)
   with col_img_org:
     left_image_placeholder = st.empty()
-  with col_img_proc:
     right_image_placeholder = st.empty()
+  with col_img_proc:
+    st.text("Hello")
+    chart_eard_placeholder.line_chart(eard_data, height=200, x_label='frame', y_label="EAR")
+
 
   if uploaded_file_vid is not None:
     with tempfile.NamedTemporaryFile(delete=False) as temp_file:
@@ -81,20 +99,20 @@ with main_tab:
       while True:  
         ret, img_source = cap.read()               
         if ret :
-          img_resize = imutils.resize(img_source, width=800)
+          img_resize = imutils.resize(img_source, width=600)
           frame_order += 1            
           file_name = os.path.join(folder_name, f'{frame_order}.png') 
-          if mode == "MediaPipe":
+          if mode == "MediaPipe": # MediaPipe mode
             img = fm.get_face_mesh(img_resize)
             left_image_placeholder.image(img_resize, channels='BGR')
             right_image_placeholder.image(img, channels='BGR')
 
-          else: # Dlib
+          else: # Dlib mode
             ear_value, img = ed.get_ear_value(img_resize)
             frame_placeholder.image(img, channels='BGR')       
             st.session_state.chart_data.append(ear_value)        
             chart_data = pd.Series(st.session_state.chart_data)
-            chart_placeholder.line_chart(chart_data, height=200, x_label='frame', y_label="EAR")
+            chart_eard_placeholder.line_chart(chart_data, height=200, x_label='frame', y_label="EAR")
             col1, col2 = columns_placeholder.columns(2)      
             col1.metric(label = 'EAR', value = int(ear_value))
             col2.metric(label = 'frame', value = frame_order )
